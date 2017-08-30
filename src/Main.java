a02afe16b117745c6e923b7f0efc61a2dc980da3
Leo <july.oloy@qq.com>
2017-08-30 17:45:28 +0800

    branch 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 
 public void Main{
+    
+    private String name = "Feature 1";
 
     public static void main(String[] args){
         System.out.println("Hello Git!");
+        System.out.println(new Main());
+    }
+
+    public String toString(){
+        return "I am " + name;
     }
 }
